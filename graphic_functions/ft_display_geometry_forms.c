4caac6745870fb486ebf928e165b0ffe4e47b66f
ysoroko <ysoroko@student.s19.be>
2021-01-29 11:43:29 +0100

    now trying to draw a square won't kill your computer. Also, the floor and the ceiling are drawn

--- graphic_functions/ft_display_geometry_forms.c
+++ graphic_functions/ft_display_geometry_forms.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 16:50:54 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/01/28 17:36:00 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/01/29 11:34:04 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void	ft_draw_square(t_square *square, t_graph *graph, int trgb)
 
 	x = square->x;
 	y = square->y;
-	while (x < square->width)
+	while (x < square->x + square->width)
 	{
 		y = square->y;
-		while (y < square->height)
+		while (y < square->y + square->height)
 		{
 			my_mlx_pixel_put(graph->img_ptr, x, y, trgb);
 			y++;
--- graphic_functions/ft_window_start.c
+++ graphic_functions/ft_window_start.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 11:20:43 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/01/29 11:29:21 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/01/29 11:39:41 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 t_graph	*ft_window_start(t_map *map)
 {
 	t_graph		*graph;
-	t_square	*square;
+	t_square	*f_square;
+	t_square	*c_square;
 
-	if (!(square = ft_new_square(0, 100, 200, 200)))
+	if (!(c_square = ft_new_square(0, 0, map->res_width, map->res_height / 2)))
 		return (0);
+	if (!(f_square = ft_new_square(0, map->res_height / 2,
+			map->res_width, map->res_height / 2)))
+	{
+		free(c_square);
+		return (0);
+	}
 	if (!(graph = ft_new_t_graph(map)))
 		return (0);
 
-	ft_draw_square(square, graph, graph->f_trgb);
+	ft_draw_square(c_square, graph, graph->c_trgb);
+	ft_draw_square(f_square, graph, graph->f_trgb);
 	mlx_put_image_to_window(graph->mlx_ptr,
 							graph->win_ptr, graph->img_ptr->img, 0, 0);
 	mlx_loop(graph->mlx_ptr);
