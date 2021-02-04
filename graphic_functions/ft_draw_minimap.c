ff614436f8de6d9fe13258d7a3cd61f52da9d63e
ysoroko <ysoroko@student.s19.be>
2021-02-04 17:41:45 +0100

    minimap speed error changed

--- graphic_functions/ft_draw_minimap.c
+++ graphic_functions/ft_draw_minimap.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 10:57:30 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/02/04 17:16:39 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/02/04 17:40:40 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,30 +37,30 @@ void		ft_draw_minimap_background(t_graph *graph)
 ** This function is used to draw the walls of the minimap
 */
 
-static void	ft_draw_minimap_walls(t_graph *graph)
+static void	ft_draw_minimap_walls(t_graph *graph, char **str_tab, double width)
 {
-	double	width;
 	int		i;
 	int		j;
 	int		x;
 	int		y;
+	double	units;
 
 	i = -1;
 	j = -1;
-	width = graph->frame->minimap_wall->width;
-	x = graph->frame->minimap_background->x;
-	y = graph->frame->minimap_background->y - graph->frame->units;
-	while (graph->map->map_str_tab[++i])
+	x = 0;
+	units = graph->frame->units;
+	y = -units;
+	while (str_tab[++i])
 	{
 		j = -1;
-		x = graph->frame->minimap_background->x;
-		y += graph->frame->units;
-		while (graph->map->map_str_tab[i][++j])
+		x = 0;
+		y += units;
+		while (str_tab[i][++j])
 		{
-			if (graph->map->map_str_tab[i][j] == '1')
+			if (str_tab[i][j] == '1')
 				ft_draw_fsquare(&(t_square){x, y, width, width},
 												graph, WALL_COLOR);
-			x += graph->frame->units;
+			x += units;
 		}
 	}
 }
@@ -75,7 +75,8 @@ static void	ft_draw_minimap_walls(t_graph *graph)
 void		ft_draw_minimap(t_graph *graph)
 {
 	ft_draw_minimap_background(graph);
-	ft_draw_minimap_walls(graph);
+	ft_draw_minimap_walls(graph, graph->map->map_str_tab,
+							graph->frame->minimap_wall->width);
 	if (!graph->circle)
 	{
 		graph->circle = ft_new_circle((graph->map->player_x *
